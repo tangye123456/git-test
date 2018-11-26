91953b54b76261882a54a8c084af5f0ebed68ab5
root <root@itcast.cn>
2018-11-26 12:56:56 +0800

    提交宿舍

---
+++ hello.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello tangye");
+        System.out.println("hello daimin");
+    }
+}
